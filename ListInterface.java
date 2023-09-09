595517bbc6a852b4bf367e511b1caa3b95fe7cff
Duy Truong <nmdtruong18032004@gmail.com>
2023-09-10 02:03:26 +0700

    test

--- ListInterface.java
+++
@@ -1,9 +0,0 @@
-public interface ListInterface<T> {
-    public boolean isEmpty();
-    public int size();
-    public T getFirst();
-    public boolean contains(T item);
-    public void addFirst(T item);
-    public T removeFirst();
-    public void print();
-}
--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        LinkedList<Integer> list=new LinkedList<>();
-        for (int i=0;i<10;i++){
-            list.addValue(i);
-        }
-        list.show();
-    }
-}
